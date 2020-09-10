0b051d90c0c99bcd29fdc441623d1a5048a561e7
Pratyaksh7 <pratyakshgupta7@gmail.com>
2020-09-10 14:28:19 +0530

    Added Comments

--- Classification/Decision Tree Classification/decision_tree_classification.py
+++ Classification/Decision Tree Classification/decision_tree_classification.py
@@ -1,28 +1,36 @@
+# importing libraries
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+# importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+# splitting the dataset into training and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
+# feature scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+# training the Decision Tree Classification model on the training set
 from sklearn.tree import DecisionTreeClassifier
 classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
 classifier.fit(X_train, y_train)
 
+# predicting a new result
 classifier.predict(sc.transform([[30, 87000]]))
 
+# predicting the test set result
 y_pred = classifier.predict(X_test)
 np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)
 
+# Making a confusion matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
 print(confusion_matrix(y_test, y_pred))
 print(accuracy_score(y_test, y_pred))
