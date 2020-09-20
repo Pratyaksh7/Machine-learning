47a9313093f2f152ef2d3d9ae76cfaad123d7eb9
Pratyaksh7 <pratyakshgupta7@gmail.com>
2020-09-20 22:55:14 +0530

    ANN

---
+++ Deep Learning/Artificial Neural Network (ANN)/artificial_neural_network.py
@@ -0,0 +1,39 @@
+# Artificial Neural Network
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+
+
+# Part1 - Data Preprocessing
+# Importing the dataset
+
+# Encoding Categrical Data
+
+# Splitting the dataset into training set and test set
+
+# Feature Scaling
+
+# Part2 - Building the ANN
+# Initializing the ANN
+
+# Adding the input layer and the first hidden layer
+
+# Adding the second hidden layer
+
+# Adding the second hidden layer
+
+# Adding the output layer
+
+# Part3 - Training the ANN
+# Compiling the ANN
+
+# Training the ANN on the training set
+
+# Making the prediction and Evaluating the model
+# Predicting the result of a single observation
+
+# Predicting the Test set results
+
+# Making the Confusion Matrix
+
