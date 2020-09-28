a58661eb3e43673c7ac2c0f5759e6a9d737afef0
Pratyaksh7 <pratyakshgupta7@gmail.com>
2020-09-28 12:48:23 +0530

    Grid Search for best hyper parameters

--- Model Selection and Boosting/Model Selection/grid_search.py
+++ Model Selection and Boosting/Model Selection/grid_search.py
@@ -1,4 +1,4 @@
-# k-Fold Cross Validation
+# Grid Search
 
 # Importing the libraries
 import numpy as np
@@ -35,8 +35,24 @@ accuracy_score(y_test, y_pred)
 # Applying k-Fold Cross Validation
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
-print("Accuracy: {:.2f} %".format(accuracies.mean() * 100))
-print("Standard Deviation: {:.2f} %".format(accuracies.std() * 100))
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+
+
+# Applying Grid Search to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV
+parameters = [{'C': [0.25, 0.5, 0.75, 1], 'kernel': ['linear']},
+              {'C': [0.25, 0.5, 0.75, 1], 'kernel': ['rbf'], 'gamma': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}]
+grid_search = GridSearchCV(estimator=classifier,
+                           param_grid=parameters,
+                           scoring='accuracy',
+                           n_jobs=-1,         # to use all the processors for fast execution
+                           cv=10)
+grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+print("Best Accuracy: {:.2f} %".format(best_accuracy * 100))
+print("Best Parameters:", best_parameters)
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
@@ -79,3 +95,7 @@ plt.show()
 #  [ 3 29]]
 # Accuracy: 90.33 %
 # Standard Deviation: 6.57 %
+
+# Grid Search
+# Best Accuracy: 90.67 %
+# Best Parameters: {'C': 0.5, 'gamma': 0.6, 'kernel': 'rbf'}
